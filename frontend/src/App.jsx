2cec87d6eb0738da23226984c1c78c03ac14efdf
Karthikeya1500 <karukarthikeya1111@gmail.com>
2026-01-29 10:40:11 +0530

    initiated signup and signin pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import SignUp from './pages/signup'
+import SignIn from './pages/signin'
+
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path ="/signup" element={<SignUp/>}/>
+        <Route path ="/signin" element={<SignIn/>}/>
+      </Routes>
     </div>
   )
 }
