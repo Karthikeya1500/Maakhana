e116f070d51d51faa86d7d3f9f62474c204183d9
Karthikeya1500 <karukarthikeya1111@gmail.com>
2026-03-22 15:39:35 +0530

    updated main to tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
 
+
+export interface IzXicYMWhProps {
+    id?: string;
+}
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { store } from './redux/store'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -12,5 +17,9 @@ createRoot(document.getElementById('root')).render(
     <App />
   </Provider>
   </BrowserRouter>
- 
+
 )
+
+export interface IzXicYMWhProps {
+    id?: string;
+}
