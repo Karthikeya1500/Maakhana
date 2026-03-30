b15667dce4ff7d367a712f4177927a40ef182256
Karthikeya1500 <karukarthikeya1111@gmail.com>
2026-03-30 17:13:00 +0530

    pointed chef routes to new controller

--- backend/routes/chef.routes.js
+++ backend/routes/chef.routes.js
@@ -1,15 +1,15 @@
 import express from "express"
-import { createEditShop, getMyShop, getShopByCity, getShopByState, getShopById, getAllShops } from "../controllers/chef.controllers.js"
+import ChefController from "../controllers/chef.controllers.js"
 import isAuth from "../middlewares/isAuth.js"
 import { upload } from "../middlewares/multer.js"
 
 const shopRouter = express.Router()
 
-shopRouter.post("/create-edit", isAuth, upload.single("image"), createEditShop)
-shopRouter.get("/get-my", isAuth, getMyShop)
-shopRouter.get("/all", getAllShops)
-shopRouter.get("/get-by-city/:city", getShopByCity)
-shopRouter.get("/get-by-state/:state", getShopByState)
-shopRouter.get("/:shopId", getShopById)
+shopRouter.post("/create-edit", isAuth, upload.single("image"), (req, res) => ChefController.createEditShop(req, res))
+shopRouter.get("/get-my", isAuth, (req, res) => ChefController.getMyShop(req, res))
+shopRouter.get("/all", (req, res) => ChefController.getAllShops(req, res))
+shopRouter.get("/get-by-city/:city", (req, res) => ChefController.getShopByCity(req, res))
+shopRouter.get("/get-by-state/:state", (req, res) => ChefController.getShopByState(req, res))
+shopRouter.get("/:shopId", (req, res) => ChefController.getShopById(req, res))
 
 export default shopRouter
