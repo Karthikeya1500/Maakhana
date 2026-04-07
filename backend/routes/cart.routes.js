cb93b7743226ab00f17bf6e2bfbd14b18a6a0b6d
Karthikeya1500 <karukarthikeya1111@gmail.com>
2026-04-07 17:09:00 +0530

    updated cart routes

--- backend/routes/cart.routes.js
+++ backend/routes/cart.routes.js
@@ -1,20 +1,14 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
-import {
-    getCart,
-    addToCart,
-    updateCartItem,
-    removeFromCart,
-    clearCart
-} from "../controllers/cart.controllers.js";
+import CartController from "../controllers/cart.controllers.js";
 
 const cartRouter = express.Router();
 
 // All cart routes require authentication
-cartRouter.get("/", isAuth, getCart);
-cartRouter.post("/add", isAuth, addToCart);
-cartRouter.put("/update", isAuth, updateCartItem);
-cartRouter.delete("/remove/:itemId", isAuth, removeFromCart);
-cartRouter.delete("/clear", isAuth, clearCart);
+cartRouter.get("/", isAuth, (req, res) => CartController.getCart(req, res));
+cartRouter.post("/add", isAuth, (req, res) => CartController.addToCart(req, res));
+cartRouter.put("/update", isAuth, (req, res) => CartController.updateCartItem(req, res));
+cartRouter.delete("/remove/:itemId", isAuth, (req, res) => CartController.removeFromCart(req, res));
+cartRouter.delete("/clear", isAuth, (req, res) => CartController.clearCart(req, res));
 
 export default cartRouter;
