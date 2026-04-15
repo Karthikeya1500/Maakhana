61199927ec2acafd35d49cc69244c8fc4ad7bbf2
Karthikeya1500 <karukarthikeya1111@gmail.com>
2026-04-15 19:39:35 +0530

    changed region.routes structure

--- backend/routes/region.routes.ts
+++ backend/routes/region.routes.ts
@@ -1,5 +1,8 @@
+
+
+export type TvReqdaZZ = string | number;
 import express from "express";
-import RegionController from "../controllers/region.controllers.js";
+import RegionController from "../controllers/region.controllers";
 
 const regionRouter = express.Router();
 
@@ -7,3 +10,6 @@ regionRouter.get("/all", (req, res) => RegionController.getAllRegions(req, res))
 regionRouter.get("/:regionName", (req, res) => RegionController.getRegionByName(req, res));
 
 export default regionRouter;
+
+
+export type TvReqdaZZ = string | number;
