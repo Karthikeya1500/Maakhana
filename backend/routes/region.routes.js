b170a9f2734c3ff527e5596a40309810feeeea43
Karthikeya1500 <karukarthikeya1111@gmail.com>
2026-04-19 15:51:00 +0530

    completed oop refactoring for region routes

--- backend/routes/region.routes.js
+++ backend/routes/region.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getAllRegions, getRegionByName } from "../controllers/region.controllers.js";
+import RegionController from "../controllers/region.controllers.js";
 
 const regionRouter = express.Router();
 
-regionRouter.get("/all", getAllRegions);
-regionRouter.get("/:regionName", getRegionByName);
+regionRouter.get("/all", (req, res) => RegionController.getAllRegions(req, res));
+regionRouter.get("/:regionName", (req, res) => RegionController.getRegionByName(req, res));
 
 export default regionRouter;
